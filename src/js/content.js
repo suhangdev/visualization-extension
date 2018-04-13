f43427e490e1d7f024e63bd8c3528945479b8da7
冥泷 <minglong@tsign.cn>
2018-04-13 18:42:35 +0800

    识别table

--- src/js/content.js
+++ src/js/content.js
@@ -1,21 +1,33 @@
 chrome.runtime.onMessage.addListener(
     function(req, sender, sendResponse) {
         console.log(req)
-        if(req.status){
-                $("body").find("*").mouseenter(function(){
-                    event.stopPropagation();
-                    $(this).css({"box-shadow":"0 0 5px 1px #3AB2FF"});
-                    $(this).one("click", function(){
-                        event.stopPropagation();
-                        $(this).hide();
-                        return false;
-                    })
-                });
-                $("body").find("*").mouseout(function(){
-                    event.stopPropagation();
-                    $(this).css("box-shadow", "none");
-                })
-        } else {
-            $("body").find("*").unbind("mouseenter").unbind("mouseout").unbind("click");
+        // if(req.status){
+        //         $("body").find("*").mouseenter(function(){
+        //             event.stopPropagation();
+        //             $(this).css({"box-shadow":"0 0 5px 1px #3AB2FF"});
+        //             $(this).one("click", function(){
+        //                 event.stopPropagation();
+        //                 // $(this).hide();
+        //                 console.log($(this).html())
+        //                 return false;
+        //             })
+        //         });
+        //         $("body").find("*").mouseout(function(){
+        //             event.stopPropagation();
+        //             $(this).css("box-shadow", "none");
+        //         })
+        // } else {
+        //     $("body").find("*").unbind("mouseenter").unbind("mouseout").unbind("click");
+        // }
+        if (req.status) {
+            let head = []
+            let data = []
+            let yAxis = []
+            let table = $('thead').parent()
+            table.css({"box-shadow":"0 0 5px 1px #3AB2FF"})
+            table.find('thead').find('th').each(function () {
+                head.push($(this).text())
+            })
+            
         }
     });
