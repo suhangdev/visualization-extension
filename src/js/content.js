1fc3a8e3fe45251ab3d054927625cdef8ed9632d
suhang <17816876697@163.com>
2018-04-15 21:58:56 +0800

    wtf

--- src/js/content.js
+++ src/js/content.js
@@ -1,33 +1,79 @@
 chrome.runtime.onMessage.addListener(
     function(req, sender, sendResponse) {
-        console.log(req)
-        // if(req.status){
-        //         $("body").find("*").mouseenter(function(){
-        //             event.stopPropagation();
-        //             $(this).css({"box-shadow":"0 0 5px 1px #3AB2FF"});
-        //             $(this).one("click", function(){
-        //                 event.stopPropagation();
-        //                 // $(this).hide();
-        //                 console.log($(this).html())
-        //                 return false;
-        //             })
-        //         });
-        //         $("body").find("*").mouseout(function(){
-        //             event.stopPropagation();
-        //             $(this).css("box-shadow", "none");
-        //         })
-        // } else {
-        //     $("body").find("*").unbind("mouseenter").unbind("mouseout").unbind("click");
-        // }
-        if (req.status) {
-            let head = []
-            let data = []
-            let yAxis = []
-            let table = $('thead').parent()
-            table.css({"box-shadow":"0 0 5px 1px #3AB2FF"})
-            table.find('thead').find('th').each(function () {
-                head.push($(this).text())
+        console.log(req);
+        body = $('body');
+        if(req.status){
+            body.find("table").css({"box-shadow":"0 0 5px 1px #3AB2FF", "cursor": "pointer"});
+            body.find("table").mouseenter(function(){
+                event.stopPropagation();
+                $(this).one("click", function(){
+                    event.stopPropagation();
+                    $(this).parent().attr('id', 'chart');
+                    $(this).parent().empty();
+                    $('#chart').css({'width': '100%', 'height': '1000px'});
+                    let chart = echarts.init(document.getElementById('chart'));
+                    let head = [];
+                    let data = [];
+                    let yAxis = [];
+                    let table = $(this);
+                    table.find('thead').find('th').each(function () {
+                        head.push($(this).text());
+                    });
+                    console.log(table.find('tbody'));
+                    table.find('tbody').each(function () {
+                        for(let i = 0; i < $(this)[0].rows.length; i++){ //遍历表格的行
+                            for(let j = 0; j < $(this)[0].rows[i].cells.length; j++){  //遍历每行的列
+                                if (j === 0) {
+                                    let html = $($(this)[0].rows[i].cells[j]);
+                                    yAxis.push(html.find('a')[0].innerHTML)
+                                } else if (j === 1) {
+                                    let arr = $(this)[0].rows[i].cells[j].innerHTML.split('<i');
+                                    $(this)[0].rows[i].cells[j].innerHTML = arr[0];
+                                    data.push(parseInt(arr[0]))
+                                }
+                            }
+                        }
+                    });
+                    console.log(yAxis);
+                    console.log(data);
+                    option = {
+                        tooltip: {
+                            trigger: 'axis',
+                            axisPointer: {
+                                type: 'shadow'
+                            }
+                        },
+                        xAxis: {
+                            type: 'value',
+                            boundaryGap: [0, 0.01],
+                            axisLabel : {//坐标轴刻度标签的相关设置。
+                                interval:0,
+                                rotate:"45"
+                            }
+                        },
+                        yAxis: {
+                            type: 'category',
+                            data: yAxis
+                        },
+                        grid: {
+                            containLabel: true
+                        },
+                        series: [
+                            {
+                                type: 'bar',
+                                data: data.reverse(),
+                            }
+                        ]
+                    };
+                    chart.setOption(option);
+                    return false;
+                })
+            });
+            body.find("*").mouseout(function(){
+                event.stopPropagation();
+                $(this).css("box-shadow", "none");
             })
-            
+        } else {
+            $("body").find("*").unbind("mouseenter").unbind("mouseout").unbind("click");
         }
     });
